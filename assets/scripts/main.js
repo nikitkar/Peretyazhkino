156c9cd971ed25203a75abda827e152fdede88d7
Nikita <nikki-xit@mail.ru>
2022-11-04 15:45:56 +0300

    header was created

---
+++ assets/scripts/main.js
@@ -0,0 +1,17 @@
+const navModileItem = document.querySelectorAll(".navModileItem");
+const navModileItemList = document.querySelectorAll(".navModileItem--List");
+let indexItem = 0;
+
+navModileItem.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        console.log(indexItem);
+        navModileItemList[indexItem].style.display = "none";
+        navModileItem[indexItem].style = "";
+
+        if (navModileItemList[index]) {
+            navModileItem[index].style = "background-color: var(--Color__Decor);color: var(--Color__White);";
+            navModileItemList[index].style.display = "flex";
+            indexItem = index;
+        }
+    });
+});
