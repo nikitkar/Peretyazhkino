502f7c21d22d0c5a6102c718b2c00b832a7d6e41
Nikita <nikki-xit@mail.ru>
2022-11-08 15:42:34 +0300

    creating reviews, advantages, questions blocks. Update styles

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,9 @@
 const navModileItem = document.querySelectorAll(".navModileItem");
 const navModileItemList = document.querySelectorAll(".navModileItem--List");
+
+const advantagesLeft = document.getElementById("advantages--Left");
+const advantagesRight = document.getElementById("advantages--Right");
+
 let indexItem = 0;
 
 navModileItem.forEach((item, index) => {
@@ -17,3 +21,33 @@ navModileItem.forEach((item, index) => {
     });
 });
 
+window.addEventListener("resize", () => {
+    if (this.innerWidth > 1130) {
+        let advantagesLeftHeight = advantagesLeft.clientHeight;
+
+        if (advantagesLeftHeight > 1103)
+            advantagesRight.style.height = advantagesLeftHeight + "px";
+        else advantagesRight.style.height = "1103px";
+    } else {
+        advantagesRight.style.height = "auto";
+        return;
+    }
+});
+
+function onToggle(event) {
+    if (event.target.open) {
+        document
+            .querySelectorAll(".questionsList > details[open]")
+            .forEach((el) => {
+                // Исключаем из перебора елемент который мы только что открыли
+                if (el === event.target) return;
+                // Закрываем все остальные елементы <details>
+                el.open = false;
+            });
+    }
+}
+
+// Вешаем наблюдатель на все елементы <details> внутри акордиона
+document
+    .querySelectorAll(".questionsItem")
+    .forEach((el) => el.addEventListener("toggle", onToggle));
