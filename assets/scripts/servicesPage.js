6445f36abf6f92a3543927dea8ed8c40cf95c52a
Nikita <nikki-xit@mail.ru>
2022-11-16 10:20:44 +0300

    creating PHP files. Update services pages. Update style, add new scripts

--- assets/scripts/servicesPage.js
+++ assets/scripts/servicesPage.js
@@ -1,8 +1,34 @@
 const mainTitleHRLeft = document.getElementById("mainTitleHRLeft");
 const mainTitleHRRight = document.getElementById("mainTitleHRRight");
 
+const mainTitleInfoTitle = document.querySelector(".mainTitleInfo--title");
+
+const containerWidth = 1454;
+const containerWidthDeviton = 727;
+const differenceValue = containerWidth - mainTitleInfoTitle.clientWidth;
+const value1 = mainTitleInfoTitle.clientWidth - differenceValue;
+const value2 = mainTitleInfoTitle.clientWidth - containerWidthDeviton;
+
 mainTitleHRLeft.style.width = window.innerWidth / 2 - 702 + "px";
-mainTitleHRRight.style.width = window.innerWidth / 2 - 420 + "px";
+mainTitleHRRight.style.width = window.innerWidth / 2 - value2 - 10 + "px";
+
+if (mainTitleInfoTitle.clientHeight > 106) {
+    const countElement = mainTitleInfoTitle.innerHTML.trim().split(" ");
+
+    const div = document.createElement("div");
+    div.classList.add("mainTitleInfo--title");
+    mainTitleInfoTitle.append(div);
+    div.innerHTML = countElement[countElement.length - 1];
+    div.style.width = "fit-content";
+    const divWidth = div.clientWidth;
+    div.style.display = "none";
+
+    mainTitleHRRight.style.top = "368px";
+    mainTitleHRRight.style.width =
+        window.innerWidth / 2 +
+        (containerWidth - divWidth - containerWidthDeviton - 10) +
+        "px";
+}
 
 if (window.innerWidth < 1445) {
     mainTitleHRLeft.style.width = "30px";
@@ -11,6 +37,8 @@ if (window.innerWidth < 1445) {
 if (window.innerWidth < 1380) {
     mainTitleHRRight.style.width =
         window.innerWidth / 2 - 420 - (1380 - window.innerWidth) / 2 + "px";
+    
+        console.log(window.innerWidth / 2 - 420 - (1380 - window.innerWidth) / 2);
 }
 
 if (window.innerWidth < 1194) {
@@ -18,7 +46,7 @@ if (window.innerWidth < 1194) {
 }
 
 if (window.innerWidth < 1100) {
-    mainTitleHRRight.style.width = window.innerWidth - 160 + "px";
+    mainTitleHRRight.style.width = window.innerWidth - mainTitleInfoTitle.clientWidth - 10 + "px";
 }
 
 const reviewsHR = document.getElementById("reviewsHR");
